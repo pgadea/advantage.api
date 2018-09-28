add1acff33fba573ffae2b0bbb80e0694f1679fe
Pierre <pgadea7@gmail.com>
2018-09-27 21:55:04 -0500

    fix GetOrder route

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -71,7 +71,7 @@ namespace Advantage.API.Controllers
             return Ok (groupedResult);
         }
 
-        [HttpGet("GetOrder/{}", Name = "GetOrder")]
+        [HttpGet("GetOrder/{id}", Name = "GetOrder")]
         public IActionResult GetOrder(int id)
         {
             var order = _ctx.Orders.Include(o => o.Customer)
